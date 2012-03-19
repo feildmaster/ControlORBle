b2231752da6e431eafe8813ac83fdcf5e9c87eb6
feildmaster <admin@feildmaster.com>
2012-03-19 16:11:03 -0500

    Added an EventTest

---
+++ src/test/java/com/feildmaster/controlorble/listeners/TestListeners.java
@@ -0,0 +1,34 @@
+package com.feildmaster.controlorble.listeners;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import junit.framework.Assert;
+import org.bukkit.event.Event;
+import org.bukkit.event.EventHandler;
+import org.junit.Test;
+
+/**
+ * After a recent blunder, I've decided to write this test to assert that all
+ * event listener functions are proper handlers.
+ *
+ * @author Feildmaster
+ */
+public class TestListeners {
+    @Test
+    public void testListeners() {
+        for (Method method : OrbListener.class.getDeclaredMethods()) {
+            // We only care about public functions.
+            if (!Modifier.isPublic(method.getModifiers())) continue;
+            // Don't mess with non-void
+            if (!Void.TYPE.equals(method.getReturnType())) continue;
+            // Only look for functions with 1 parameter
+            if (method.getParameterTypes().length != 1) continue;
+
+            // This is an event function...
+            if (Event.class.isAssignableFrom(method.getParameterTypes()[0])) {
+                // Make sure @EventHandler is present!
+                Assert.assertTrue(method.getName()+" is missing @EventHandler!", method.isAnnotationPresent(EventHandler.class));
+            }
+        }
+    }
+}
