57b43a3678fe4be63378e4610fca4aa9c773551f
feildmaster <admin@feildmaster.com>
2012-05-22 05:32:50 -0500

    Add /exp <player> and a few formatting fixes.

--- src/main/java/com/feildmaster/controlorble/commands/ExpCommand.java
+++ src/main/java/com/feildmaster/controlorble/commands/ExpCommand.java
@@ -22,10 +22,15 @@ public class ExpCommand implements CommandExecutor {
         }
 
         int size = args.length;
-        if(size == 0) return yourLevel(sender);
-        if(size > 2) return invalidCommand(sender, label);
+        if (size == 0) {
+            return yourLevel(sender);
+        } else if (size > 2) {
+            return invalidCommand(sender, label);
+        }
 
-        if(!sender.hasPermission("orbEnhance.admin")) return noPermission(sender);
+        if(!sender.hasPermission("orbEnhance.admin")) {
+            return noPermission(sender);
+        }
 
         Player p1 = null;
         Integer exp = null;
@@ -33,9 +38,14 @@ public class ExpCommand implements CommandExecutor {
         String format2 = "You have been given %1$s experience";
         String parse = null;
 
-        if(size == 1 && sender instanceof Player) {
-            p1 = (Player)sender;
-            parse = args[0];
+        if (size == 1) {
+            p1 = plugin.getServer().getPlayer(args[0]);
+            if (p1 != null) {
+                return playerLevel(sender, p1);
+            } else if (sender instanceof Player) {
+                p1 = (Player) sender;
+                parse = args[0];
+            }
         } else if (size == 2) {
             p1 = sender.getServer().getPlayer(args[0]);
             parse = args[1];
@@ -43,7 +53,9 @@ public class ExpCommand implements CommandExecutor {
             return invalidCommand(sender, label);
         }
 
-        if(p1==null) return playerNotFound(sender);
+        if (p1==null) {
+            return playerNotFound(sender);
+        }
 
         Editor p2 = new Editor(p1);
 
@@ -60,7 +72,7 @@ public class ExpCommand implements CommandExecutor {
             return notANumber(sender);
         }
 
-        if(parse.startsWith("+")||parse.startsWith("-")) {
+        if (parse.startsWith("+") || parse.startsWith("-")) {
             format = String.format(format2, exp.toString());
             // Is this supposed to be here...?
             //sender.sendMessage("Exp: "+ exp + " + " + p2.getTotalExp(true) + " = " + (exp+p2.getTotalExp()));
@@ -72,8 +84,8 @@ public class ExpCommand implements CommandExecutor {
         p2.setExp(exp);
 
         // Sender console, or player is a different player
-        if(!(sender instanceof Player) || !((Player)sender).equals(p1)) {
-            p1.sendMessage(plugin.format(ChatColor.YELLOW,format));
+        if (!(sender instanceof Player) || !((Player)sender).equals(p1)) {
+            p1.sendMessage(plugin.format(ChatColor.YELLOW, format));
         }
 
         sender.sendMessage(plugin.format("Player experience changed from "+old_lvl+"/"+old_exp+" to "+p2.getLevel()+"/"+p2.getExp()));
@@ -81,7 +93,9 @@ public class ExpCommand implements CommandExecutor {
     }
 
     private boolean onReloadCommand(CommandSender sender) {
-        if(!sender.hasPermission("orbEnhance.admin")) return noPermission(sender);
+        if (!sender.hasPermission("orbEnhance.admin")) {
+            return noPermission(sender);
+        }
         plugin.getConfig().load();
         plugin.checkConfig();
         sender.sendMessage(plugin.format("Reload Complete"));
@@ -92,33 +106,42 @@ public class ExpCommand implements CommandExecutor {
         sender.sendMessage(plugin.format("You do not have permission to do that!"));
         return true;
     }
+
     private boolean playerNotFound(CommandSender sender) {
         sender.sendMessage(plugin.format("Player not found"));
         return true;
     }
+
     private boolean invalidCommand(CommandSender sender, String label) {
         sender.sendMessage(plugin.format("Invalid syntax"));
         sender.sendMessage(plugin.format("/"+label+" [player] [+|-]{exp}"));
         return true;
     }
+
     private boolean notANumber(CommandSender sender) {
         sender.sendMessage(plugin.format("Not a number"));
         return true;
     }
 
     private boolean yourLevel(CommandSender sender) {
-        if(sender instanceof Player) {
-            Editor p = new Editor((Player)sender);
-            int level = p.getLevel();
-
-            sender.sendMessage(plugin.format("Your level: "+level));
-            sender.sendMessage(plugin.format("Experience: "+p.getExp()+"/"+p.getExpToLevel()));
-
-            if(plugin.getConfig().getBoolean("config.showTotal")) {
-                sender.sendMessage(plugin.format("Total Exp : "+p.getTotalExp(true)));
-            }
-        } else
+        if (sender instanceof Player) {
+            return playerLevel(sender, (Player) sender);
+        } else {
             invalidCommand(sender, "exp");
+        }
+        return false;
+    }
+
+    private boolean playerLevel(CommandSender sender, Player player) {
+        Editor p = new Editor(player);
+
+        sender.sendMessage(plugin.format((sender.equals(player) ? "Your" : (player.getName() + "'s")) + " level: " + p.getLevel()));
+        sender.sendMessage(plugin.format("Experience: " + p.getExp()+ "/" + p.getExpToLevel()));
+
+        if(plugin.getConfig().getBoolean("config.showTotal")) {
+            sender.sendMessage(plugin.format("Total Exp : " + p.getTotalExp(true)));
+        }
+
         return true;
     }
 }
